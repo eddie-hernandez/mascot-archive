df8f5f8d92b750419bfa9a2c1030b2e856ed78a5
eddie-hernandez <71785228+eddie-hernandez@users.noreply.github.com>
2023-09-04 01:28:42 -0400

    fixing re-render loop on gallery resize

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -1,78 +1,73 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import Photo from '../photo/Photo'
-import './Gallery.css'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Photo from '../photo/Photo';
+import './Gallery.css';
 
 export default function Gallery({ images }) {
-  const imageWidth = 150
-  const imageHeight = 150
-  // adjust value to control the spacing between images
-  const margin = 20
+  const imageWidth = 150;
+  const imageHeight = 150;
+  const margin = 20;
 
-  const [placedImages, setPlacedImages] = useState([])
+  const [placedImages, setPlacedImages] = useState([]);
 
   useEffect(() => {
-    // Check if placedImages is empty and resetPlacement is true to re-randomize positions
-    const maxX = window.innerWidth - imageWidth
-    const maxY = window.innerHeight - imageHeight
-    const initialY = margin // Initial vertical position
-    let currentY = initialY // Current vertical position
+    const maxX = window.innerWidth - imageWidth - margin;
+    const maxY = window.innerHeight - imageHeight;
 
-    const newPlacedImages = []
-    images.forEach((image) => {
-      let newRandomX, newRandomY
+    const shuffledImages = [...images].sort(() => Math.random() - 0.5);
+
+    const newPlacedImages = [];
+
+    shuffledImages.forEach((image, index) => {
+      let newRandomX, newRandomY;
+      let attempts = 0;
+
+      // keep generating new X-coordinate until it's within screen boundaries
       do {
-        newRandomX = Math.random() * (maxX - imageWidth - margin) + margin
-        newRandomY = currentY // Use the currentY value for vertical position
+        newRandomX = Math.random() * maxX;
+        newRandomY = Math.random() * maxY;
+
+        attempts++;
+
+        // if it takes too many attempts, break to prevent infinite loop
+        if (attempts > 100) {
+          break;
+        }
       } while (
         newPlacedImages.some(
           ({ x, y }) =>
             Math.abs(newRandomX - x) < imageWidth &&
             Math.abs(newRandomY - y) < imageHeight
         )
-      )
+      );
 
-      newPlacedImages.push({ x: newRandomX, y: newRandomY })
+      newPlacedImages.push({ x: newRandomX, y: newRandomY, id: image._id });
+    });
 
-      // Increment the currentY value to space images vertically
-      currentY += imageHeight + margin
-    })
-
-    setPlacedImages(newPlacedImages)
-  }, [images])
-
-  // REGUALAR POSITIONS
-  //   return (
-  //     <div className="gallery-container">
-  //       {images.map((image, index) => {
-  //         return (
-  //           <Link to={`mascot/${image._id}`} key={image._id}>
-  //             <Photo image={image} alt={`Mascot ${index}`} />
-  //           </Link>
-  //         )
-  //       })}
-  //     </div>
-  //   )
-  // }
+    setPlacedImages(newPlacedImages);
+  }, [images]);
 
   return (
     <div className="gallery-container">
-      {/* map thru images to compare with placedImages array */}
       {images.map((image, index) => {
-        // check if placedImages[index] is defined before accessing its properties
-        const imageStyle = placedImages[index]
-          ? {
-              left: `${placedImages[index].x}px`,
-              top: `${placedImages[index].y}px`,
-            }
-          : {}
+        const placedImage = placedImages.find((pi) => pi.id === image._id);
+
+        if (!placedImage) {
+          // handle images not yet placed
+          return null;
+        }
+
+        const imageStyle = {
+          left: `${placedImage.x}px`,
+          top: `${placedImage.y}px`,
+        };
 
         return (
           <Link to={`/mascot/${image._id}`} key={image._id}>
-              <Photo image={image} alt={`Mascot ${index}`} style={imageStyle} />
+            <Photo image={image} alt={`Mascot ${index}`} style={imageStyle} />
           </Link>
-        )
+        );
       })}
     </div>
-  )
+  );
 }
