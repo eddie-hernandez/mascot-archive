0ed893a741122c760a3eb50124f134ba38558c96
eddie-hernandez <71785228+eddie-hernandez@users.noreply.github.com>
2023-09-04 13:36:29 -0400

    reconfiguring case sensitivities

--- src/components/privateRoute.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Outlet, Navigate } from 'react-router-dom' // Import Routes
-import { useSelector } from 'react-redux'
-
-export default function PrivateRoute() {
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
-
-  return isAuthenticated ? <Outlet /> : <Navigate to="/admin/login" />
-}
