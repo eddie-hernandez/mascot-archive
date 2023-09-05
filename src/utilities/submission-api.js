a162c1e66e43fce1c6286cdf09124875f55853bf
eddie-hernandez <71785228+eddie-hernandez@users.noreply.github.com>
2023-09-04 23:38:11 -0400

    fixing api endpoint for submissions

--- src/utilities/submission-api.js
+++ src/utilities/submission-api.js
@@ -1,11 +1,11 @@
 import sendRequest from './send-request';
 
-const BASE_URL = ''
+const BASE_URL = '/api/submit'
   // process.env.NODE_ENV === 'production'
   //   ? process.env.REACT_APP_DEPLOYMENT_BACKEND
   //   : process.env.REACT_APP_DEVELOPMENT_BACKEND
 
 // submit photo
 export function submitPhoto(formData) {
-  return sendRequest(BASE_URL + '/api/submit', 'POST', formData);
+  return sendRequest(BASE_URL, 'POST', formData);
 }
